b68b3f3bdbb188f73715e60dedee4a9f7f0f9d33
Eduardo Bizzi Martins <eduardobizzi2005@gmail.com>
2024-06-10 21:01:21 -0300

    CORS fix

--- app.py
+++ app.py
@@ -1,26 +1,31 @@
-from flask import Flask
-
-from database.db import *
+from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+cors = CORS(app, resources={r"/user/*": {"origins": "http://localhost:3000"}})
 
 @app.route('/')
+@app.route('/home')
 def index():
-    return 0
+    return "Sim"
 
 
-@app.route('/user/create', methods = ['GET', 'POST'])
+@app.route('/user/create', methods=['GET', 'POST'])
 def userCreate():
-    return 0
+    data = request.json
+    name = data.get('name')
+    email = data.get('email')
+    password = data.get('password')
+
+    return {}
 
 
-@app.route('/user/read', methods = ['GET'])
+@app.route('/user/read', methods=['GET'])
 def userRead():
     return 0
 
 
-@app.route('/user/update', methods = ['GET', 'PUT']) # Verificar a viabilidade de PUT ou PATCH
+@app.route('/user/update', methods=['GET', 'PUT']) # Verificar a viabilidade de PUT ou PATCH
 def userUpdate():
     return 0
 
@@ -28,3 +33,7 @@ def userUpdate():
 @app.route('/user/delete', methods = ['GET'])
 def userDelete():
     return 0
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
